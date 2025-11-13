551ba878950021019ce8f0f8382dfa7a67098660
Alcstr <johan.castro@campusucc.edu.co>
2025-11-13 18:22:38 -0500

    Update del archivo main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import App from "./App";
 import "./index.css";
-=======
-import App from "./App.tsx";
->>>>>>> 022c6481d8ca54b3677cc95f30c93e7af7868f6b
+import { AccessibilityProvider } from "./AccessibilityContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AccessibilityProvider>
+      <App />
+    </AccessibilityProvider>
   </React.StrictMode>
 );
